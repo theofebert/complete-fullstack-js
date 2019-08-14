9155f8ed20128fd32a828e90d7442b2c8ad337a4
theofebert <theodorefebert@gmail.com>
2019-08-14 23:52:01 +0700

    Create Front End using  EJS

--- server.js
+++ server.js
@@ -4,41 +4,40 @@ import config from './config';
 // Steps 2.3_P
 import apiRouter from './api';
 
-// Steps 2.3_I
-// Steps 2.3_L :
-//import fs from 'fs';
-
 // Steps 2.3_A
 import express from 'express';
 
 // Steps 2.3_B
 const server = express();
 
+// Steps 2.4_A
+server.set('view engine', 'ejs');
+
 // Steps 2.3_F
+// server.get('/', (req, res) => {
+//   res.send('Hello Express!');
+// });
+
+// Steps 2.4_C
+// server.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// Steps 2.4_D
 server.get('/', (req, res) => {
-  res.send('Hello Express!');
+  res.render('index', {
+    // content: 'Hello Express with EJS',
+
+    // Steps 2.4_F
+    content: 'Hello Express with <em>EJS!</em>',
+  });
 });
-
-// Steps 2.3_G
-// server.get('/about.html', (req, res) => {
-//   res.send('The About Page');
-// });
-
-// Steps 2.3_J
-// server.get('/about.html', (req, res) => {
-//   fs.readFile('./about.html', (err, data) => {
-//       res.send(data.toString());
-//     });
-// });
 
 // Steps 2.3_Q
 server.use('/api', apiRouter);
 
 // Steps 2.3_K : simply magis middleware
 server.use(express.static('public'));
-
-// Steps 2.3_C
-// server.listen();
 
 // Steps 2.3_E
 server.listen(config.port, () => {
