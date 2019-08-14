819c651af5f513d2946195824142d0a3de312f08
theofebert <theodorefebert@gmail.com>
2019-08-14 14:39:50 +0700

    Import files use node middleware

---
+++ server.js
@@ -0,0 +1,23 @@
+// Steps 2.1_A
+// import './config';
+
+// Steps 2.1_F
+// import config from './config';
+
+// Steps 2.1_J
+// import config, { nodeEnv } from './config';
+
+// Steps 2.1_M
+import config, { nodeEnv, logStars } from './config';
+
+// Steps 2.1_D
+// console.log(env);
+
+// Steps 2.1_G
+// console.log(config);
+
+// Steps 2.1_K
+console.log(config, nodeEnv);
+
+// Steps 2.1_N
+logStars('function');
